85d9cfcbadcd4c0f1c148d6d559341be5422df23
megha yadav <meghayadav5216@gmail.com>
2025-11-06 11:43:59 +0530

    first upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('sign_up/', views.sign_up, name = 'signup' ),
     path('accounts/', include('django.contrib.auth.urls') ),#builtin log in , change password
     path('home/', views.home, name = 'home' )
