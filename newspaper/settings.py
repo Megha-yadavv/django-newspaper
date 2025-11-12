206c5160a6990128da2833be86beb8dc0c4ecbb3
megha yadav <meghayadav5216@gmail.com>
2025-11-12 14:28:35 +0530

    first upload

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+STATICFILES_DIRS = [
+    BASE_DIR, 'static'
+]
 
 LANGUAGE_CODE = 'en-us'
 
